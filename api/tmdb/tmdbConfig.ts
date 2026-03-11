262ac289b9e8bc9ed8d61f21f6787a1dd26f5656
Cedro23 <cedric_lesueur@outlook.fr>
2026-03-11 12:18:14 +0100

    Fix env variables

--- api/tmdb/tmdbConfig.ts
+++ api/tmdb/tmdbConfig.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import Config from "react-native-config";
 
 export const tmdbAxios = axios.create({
-  baseURL: Config.TMDB_API_BASE_URL,
+  baseURL: process.env.EXPO_PUBLIC_TMDB_API_BASE_URL,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${Config.TMDB_API_READ_KEY}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_READ_KEY}`,
   },
 });
