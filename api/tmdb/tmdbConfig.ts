710d2be463e384b85525b38883f87f940c870dc5
Cedro23 <cedric_lesueur@outlook.fr>
2026-03-11 11:38:50 +0100

    Using config files and updated API key

--- api/tmdb/tmdbConfig.ts
+++ api/tmdb/tmdbConfig.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
-
-const TMDB_API_KEY = "";
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+import Config from "react-native-config";
 
 export const tmdbAxios = axios.create({
-  baseURL: TMDB_BASE_URL,
+  baseURL: Config.TMDB_API_BASE_URL,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${TMDB_API_KEY}`,
+    Authorization: `Bearer ${Config.TMDB_API_READ_KEY}`,
   },
 });
